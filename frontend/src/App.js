7f510bebfd26151325c44340c3a115ab8a115afc
enriqueSFranco <enrique.sfranco04@gmail.com>
2022-02-07 15:14:21 -0600

    proyecto limpio

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import Form from "./components/Form";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>App</h1>
+      <Form />
     </div>
   );
 }
---
+++ frontend/src/hooks/useForm.js
@@ -0,0 +1,27 @@
+import { useState } from "react";
+
+export const useForm = (initialForm, validateForm) => {
+  const[form, setForm] = useState(initialForm);
+  const[errors, setErrors] = useState({});
+  const[loading, setLoading] = useState(false);
+  const[response, setResponse] = useState(null);
+
+  const handleChange = (e) => {
+    const {name, value} = e.target;
+    setForm({
+      ...form,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = () => {};
+
+  return {
+    form,
+    errors,
+    loading,
+    response,
+    handleChange,
+    handleSubmit
+  };
+};
