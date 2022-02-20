8bd55de212d50a4b5401f6f445226ea924a0924e
enriqueSFranco <enrique.sfranco04@gmail.com>
2022-02-19 20:14:28 -0600

    se agrego el manejo de rutas

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,40 @@
-import Form from "./components/Form";
+import { 
+  Routes,
+  Route,
+} from "react-router-dom";
+import PrivateRoute from "./routes/PrivateRoute";
+
+import Header from "./components/Menu/Header";
+import PageHome from "./pages/PageHome";
+import PageCompany from "./pages/PageCompany";
+import PageLogin from "./pages/PageLogin";
+import PageCreateAccount from "./pages/PageCreateAccount";
+import PageRecruiter from "./pages/PageRecruiter";
+import { AuthProvider } from "./context/AuthContext";
+import PageDetailsUser from "./pages/PageDetailsUser";
+// import PageDashBoard from "./pages/PageDashBoard";
+
+import "./App.css";
 
 function App() {
   return (
-    <div>
-      <h1>App</h1>
-      <Form />
-    </div>
+    <>
+      <AuthProvider>  
+        <Header />
+        <Routes>
+          {/* Rutas publicas */}
+          <Route path="/" element={<PageHome />}/>
+          <Route path="/empresas" element={<PageCompany />} />
+            <Route path="/nueva-cuenta" element={<PageCreateAccount />} />
+          <Route path="/iniciar-sesion" element={<PageLogin />} />
+          {/* Rutas privadas */}
+          <Route element={<PrivateRoute />}>
+            <Route path="/reclutador" element={<PageRecruiter />} />
+            <Route path="/configuracion" element={<PageDetailsUser />} />
+          </Route>
+        </Routes>
+      </AuthProvider>
+    </>
   );
 }
 
