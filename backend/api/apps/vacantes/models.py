8c684b7149335b9fdc7034c6dc4dbc64f23e40b8
Iris Martinez <irisyonitzimartinezgonzalez@outlook.com>
2022-03-01 23:43:39 -0600

    Add: Models-Vacant

--- backend/api/apps/vacantes/models.py
+++ backend/api/apps/vacantes/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+#from simple_history.models import HistoricalRecords
 
 # Create your models here.
+class Vacant:
+    t200_id_vacante = models.AutoField(primary_key=True)
+    t200_puesto = models.CharField(max_length=50)
+    t200_descripcion = models.TextField()
+    t200_ubicacion = models.TextField()
+    t200_hora_entrada = models.DateField()
+    t200_hora_salida = models.DateField()
+    t200_dias_laborales = models.CharField(max_length=7)
+    t200_prestaciones = models.CharField()
+    t200_pago_min = models.IntegerField()
+    t200_pago_max = models.IntegerField()
+    t200_pago_bruto = models.BooleanField()
+    t200_remoto = models.BooleanField()
+    t200_fecha_cierre = models.DateField()
+    t200_fecha_alta = models.DateField()
+    #t200_puestos
+
