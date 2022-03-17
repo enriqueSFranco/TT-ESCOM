f9cfaffacb0a52ba74314123d93e891363597560
enriqueSFranco <enrique.sfranco04@gmail.com>
2022-03-17 00:32:00 -0600

    componente CardJobList

--- frontend/src/components/Card/CardJobList.js
+++ frontend/src/components/Card/CardJobList.js
@@ -1,18 +1,26 @@
 import { Link, Outlet } from "react-router-dom";
+import { useFetch } from "../../hooks/useFetch";
 import JobCard from "./CardJob";
-import db from "../../api/db.json";
 import styles from "./CardJob.module.css";
 
 const JobList = () => {
+  const { data } = useFetch("/empleos");
+
+
+  console.log(data)
+
+  if (!data) return null;
+
   return (
     <div className={styles.grid}>
       <div className={`${styles.wrapper}`}>
-        {db.map(
+        {data.map(
           ({
             id,
             company,
             img_company,
             type_vacancy,
+            descriptionJob,
             min_salary,
             max_salary,
             full_time,
@@ -22,6 +30,7 @@ const JobList = () => {
               <JobCard
                 company={company}
                 type_vacancy={type_vacancy}
+                descriptionJob={descriptionJob}
                 min_salary={min_salary}
                 max_salary={max_salary}
                 full_time={full_time}
