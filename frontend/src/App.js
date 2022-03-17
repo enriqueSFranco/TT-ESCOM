df48b1e669cae573dde0cdf1100ecfa8e7e6617a
enriqueSFranco <enrique.sfranco04@gmail.com>
2022-03-17 13:34:56 -0600

    app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,7 @@
-import Header from "./components/Menu/Header";
-import Root from "./Root";
 import React from "react"
-
+import Root from "./Root";
+import Header from "./components/Menu/Header";
 import { AuthProvider } from "./context/AuthContext";
-
-import "./App.css";
 
 function App() {
 
