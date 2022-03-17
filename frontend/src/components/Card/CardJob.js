d84b4c58ecdc8930200f24c78bccb9f1cf4c53f4
enriqueSFranco <enrique.sfranco04@gmail.com>
2022-03-17 13:35:38 -0600

    refactorizacion

--- frontend/src/components/Card/CardJob.js
+++ frontend/src/components/Card/CardJob.js
@@ -1,37 +1,25 @@
 import React from "react";
 import styles from "./CardJob.module.css";
 
-const JobCard = ({
-  company,
-  type_vacancy,
-  descriptionJob,
-  min_salary,
-  maxSalary,
-  full_time,
-  locationJob,
-}) => {
-
+const JobCard = ({ job }) => {
   return (
     <article className={`${styles.card}`}>
       <header className={styles.cardHeader}>
-          <h3 className={`${styles.nameCompany} ${styles.placeholder}`}>
-            {company}
-          </h3>
-          <h3 className={`${styles.title} ${styles.placeholder}`}>
-            {type_vacancy}
-          </h3>
+        <h3 className={`${styles.nameCompany}`}>
+          {job?.company}
+        </h3>
       </header>
       <main>
-        <p className={`${styles.lineClamp}  ${styles.placeholder}`}>
-        {descriptionJob}
+        <p className={`${styles.lineClamp}`}>
+          {job?.descriptionJob}
         </p>
-        <p className={`${styles.placeholder} ${styles.timeWork}`}>
-          Tiempo: {full_time ? "Tiempo Completo" : "Medio Tiempo"}
+        <p className={`${styles.timeWork}`}>
+          Tiempo: {job?.full_time ? "Tiempo Completo" : "Medio Tiempo"}
         </p>
-        <p className={`${styles.placeholder} ${styles.location}`}>
-          Ubicacion: {locationJob}
+        <p className={`${styles.location}`}>
+          Ubicacion: {job?.locationJob}
         </p>
-        <p className={`${styles.placeholder} ${styles.publicationTime}`}>
+        <p className={`${styles.publicationTime}`}>
           Publicada hace: 1min
         </p>
       </main>
