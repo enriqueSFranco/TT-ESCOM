98683edc11ee6e4e8dbe2d3b8c5ff2f932dc38d9
Iris Martinez <irisyonitzimartinezgonzalez@outlook.com>
2022-03-18 16:52:32 -0600

    ResolConflic

--- frontend/src/pages/PageProfileStudent.js
+++ frontend/src/pages/PageProfileStudent.js
@@ -1,9 +1,7 @@
+import React from 'react';
 import CardProfileStudent from "../components/Card/CardProfileStudent";
-<<<<<<< HEAD:frontend/src/pages/PageProfileStudent.js
 import styles from "./PageProfileStudent.module.css";
-=======
 import CardDetailsStudent from "../components/AboutMe/AboutMe";
->>>>>>> be2c133aa9d6ef8ff503384a7ae4eaaeec025b1d:frontend/src/pages/PageDetailsUser.js
 
 const PageProfileStudent = () => {
   return (
