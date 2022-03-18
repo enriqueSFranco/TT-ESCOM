1fe87b51d269dd54d448719be0bfbb80ce2a4e7c
enriqueSFranco <enrique.sfranco04@gmail.com>
2022-03-18 15:52:05 -0600

    formulario y estilos para actualizar la informacion de un alumno

--- frontend/src/components/Form/FormUpdateDataStudent.js
+++ frontend/src/components/Form/FormUpdateDataStudent.js
@@ -1,5 +1,4 @@
-import React from "react"
-import { useParams } from "react-router-dom";
+import React from "react";
 import { useForm } from "../../hooks/useForm";
 import Label from "../Input/Label";
 import Input from "../Input/Input";
@@ -7,13 +6,14 @@ import Span from "../Input/Span";
 import Switch from "../Input/Switch";
 import * as BiIcon from "react-icons/bi";
 import styles from "./FormUpdateDataStudent.module.css";
+import { motion } from "framer-motion/dist/framer-motion";
 
 let initialForm = {
   name: "",
   specialty: "",
   phone: undefined,
   location: "",
-  travel: undefined,
+  travel: false,
   cv: undefined,
   socialNetworks: {
     email: undefined,
@@ -23,61 +23,62 @@ let initialForm = {
   },
 };
 
-
-const FormUpdateDataStudent = ({ ref }) => {
+const FormUpdateDataStudent = ({ handleBackToProfile }) => {
   const { form, handleChange, handleChecked } = useForm(initialForm);
-  const { name } = useParams();
+
   return (
     <>
-      <div className={styles.wrapper}>
+      <motion.div
+        className={styles.wrapper}
+        initial={{ scaleY: 0 }}
+        animate={{ scaleY: 1 }}
+        exit={{ scaleY: 0 }}
+        duration={{ duration: 0.5 }}
+      >
         <h1 className={styles.title}>editar informacion</h1>
         <form>
-          <div>
-            <Label htmlFor="name">
+          <Label htmlFor="name">
+            <Input
+              type="text"
+              id="name"
+              name="name"
+              placeholder=" "
+              value={form.name}
+              onChange={handleChange}
+            />
+            <Span content="Nombre" />
+          </Label>
+          <Label htmlFor="specialty">
+            <Input
+              type="text"
+              id="specialty"
+              name="specialty"
+              placeholder=" "
+              value={form.specialty}
+              onChange={handleChange}
+            />
+            <Span content="Puesto" />
+          </Label>
+          <Label htmlFor="location">
               <Input
                 type="text"
-                id="name"
-                name="name"
+                name="location"
+                id="location"
                 placeholder=" "
-                value={form.name}
+                value={form.location}
                 onChange={handleChange}
               />
-              <Span content="Nombre"/>
+              <Span content="Donde vives" />
             </Label>
-            <Label htmlFor="specialty">
-              <Input
-                type="text"
-                id="specialty"
-                name="specialty"
-                placeholder=" "
-                value={form.specialty}
-                onChange={handleChange}
-              />
-              <Span content="Puesto" />
-            </Label>
-          </div>
-          <div className={`${styles.inputGroup}`}>
-            <div className="col">
-              <Label htmlFor="location">
-                <Input
-                  type="text"
-                  name="location"
-                  id="location"
-                  placeholder=" "
-                  value={form.location}
-                  onChange={handleChange}
-                />
-                <Span content="Donde vives" />
-              </Label>
-            </div>
-            <div className="col">
-              <Switch name="travel" id="travel" value={form.travel} onChange={handleChecked} />
-            </div>
-          </div>
-          <div>
+          <Switch
+              label="Disponibilidad para viajar"
+              name="travel"
+              id="travel"
+              value={form.travel}
+              onChange={handleChecked}
+            />
             <Label htmlFor="phone">
-              <input
-                className={styles.inputField}
+              <Input
                 type="tel"
                 name="phone"
                 id="phone"
@@ -85,26 +86,23 @@ const FormUpdateDataStudent = ({ ref }) => {
                 value={form.phone}
                 onChange={handleChange}
               />
-              <span className={styles.inputLabel}>Telefono/Whatsapp</span>
+              <Span content="Telefono/Whatsapp" />
             </Label>
-          </div>
-          <div>
-            <input
-              type="file"
-              name="cv"
-              id="cv"
-              className={`${styles.inputFile}`}
-              value={form.cv}
-              onChange={handleChange}
-            />
-            <Label htmlFor="cv">
-              <BiIcon.BiCloudUpload />
-              subir cv
-            </Label>
-          </div>
+                <input
+                  type="file"
+                  name="cv"
+                  id="cv"
+                  className={`${styles.inputFile}`}
+                  value={form.cv}
+                  onChange={handleChange}
+                />
+              <Label htmlFor="cv">
+                <BiIcon.BiCloudUpload />
+                subir cv
+              </Label>
           <div>{/* links de redes sociales */}</div>
         </form>
-        <div className="container w-100 my-5">
+        <div className="container w-100">
           <div className="row">
             <div className="col">
               <button className="btn btn-outline-primary w-100 my-1">
@@ -114,7 +112,7 @@ const FormUpdateDataStudent = ({ ref }) => {
               </button>
             </div>
             <div className="col">
-              <button className="btn btn-outline-danger w-100 my-1" ref={ref}>
+              <button onClick={handleBackToProfile} className="btn btn-outline-danger w-100 my-1">
                 <div className="row align-items-center">
                   <div className="col-12 col-md-10 text-center">Cancelar</div>
                 </div>
@@ -122,7 +120,7 @@ const FormUpdateDataStudent = ({ ref }) => {
             </div>
           </div>
         </div>
-      </div>
+      </motion.div>
     </>
   );
 };
