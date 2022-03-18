607e22f2b4b3594b821b0dad51436b7a107750a6
enriqueSFranco <enrique.sfranco04@gmail.com>
2022-03-18 17:12:13 -0600

    resolve fix

--- frontend/src/pages/PageCompany.js
+++ frontend/src/pages/PageCompany.js
@@ -3,17 +3,9 @@ import React from "react"
 
 const PageCompany = () => {
   return (
-<<<<<<< HEAD
-    <div  className="row align-items-stretch my-5">
-      <div><Objects></Objects></div>
-      <div><Interests></Interests>
-      <Skills></Skills></div>
-      <div><Languajes></Languajes></div>
-=======
     <div className="row align-items-stretch my-5">
 
       
->>>>>>> be2c133aa9d6ef8ff503384a7ae4eaaeec025b1d
     </div>
     
   )
