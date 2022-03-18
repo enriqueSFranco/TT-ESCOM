58e582f6a5d166623d87a52ec7bf08f7e3b8cfb3
enriqueSFranco <enrique.sfranco04@gmail.com>
2022-03-18 15:53:34 -0600

    componente y estilos del elemento switch

--- frontend/src/components/Input/Switch.js
+++ frontend/src/components/Input/Switch.js
@@ -1,14 +1,21 @@
-import React from 'react'
+import styles from "./Styles.module.css";
 
-const Switch = ({ name, id, value, onChange }) => {
+const Switch = ({ label, name, id, value, onChange }) => {
   return (
-    <input 
-      type="checkbox" 
-      name={name}
-      id={id} 
-      value={value} 
-      onChange={onChange} 
-    />
+    <>
+      <label className={styles.wrapperSwitch}>
+        <input
+          className={styles.inputSwitch}
+          type="checkbox" 
+          name={name}
+          id={id} 
+          value={value} 
+          onChange={onChange} 
+        />
+        <span className={`${styles.sliderSwitch} ${styles.check}`}></span>
+      <span className={styles.label}>{label}</span>
+      </label>
+    </>
   )
 };
 
