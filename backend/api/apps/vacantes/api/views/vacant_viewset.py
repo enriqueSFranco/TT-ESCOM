f50a7e6db2c85c8f76996fad3dd0d67af862bb40
JoelHeras <joel_kratos@hotmail.com>
2022-03-27 16:20:30 -0600

    Fix

--- backend/api/apps/vacantes/api/views/vacant_viewset.py
+++ backend/api/apps/vacantes/api/views/vacant_viewset.py
@@ -17,27 +17,18 @@ class VacantViewSet(viewsets.GenericViewSet):
 	def get_object(self, pk):	
 		self.queryset = self.model.objects\
 				.filter(t200_id_vacant = pk)\
-<<<<<<< HEAD
-				.values('t200_id_vacant','t300_id_company','t200_job','t200_description','t200_check_time','t200_closing_hour','t200_work_days',
-				'c207_id_experience','t200_min_salary','t200_max_salary','t200_gross_salary','t200_home_ofice','c204_id_vacant_status','t200_publish_date','t200_close_date','t301_id_recruiter','t400_id_admin')
-=======
 				.all()
 				#values('t200_id_vacant','t200_job','t200_description','t200_check_time','t200_closing_hour','t200_work_days',
             #'t200_min_salary','t200_max_salary','t200_gross_salary','t200_home_ofice','t200_publish_date','t200_close_date')		
->>>>>>> feature/Publicar-vacantes
 		return self.queryset
 
 	def get_queryset(self):
 		if self.queryset is None:
-			self.queryset = self.model.objects\
+			self.queryset = self.model.objects\ 
 				.filter()\
 				.values('t200_id_vacant','t300_id_company','t200_job','t200_description','t200_check_time','t200_closing_hour','t200_work_days',
-<<<<<<< HEAD
-				'c207_id_experience','t200_min_salary','t200_max_salary','t200_gross_salary','t200_home_ofice','c204_id_vacant_status','t200_publish_date','t200_close_date','t301_id_recruiter','t400_id_admin')
-=======
 				'c207_id_experience','t200_min_salary','t200_max_salary','t200_gross_salary','t200_home_ofice','c204_id_vacant_status','t200_publish_date',
 				't200_close_date','t301_id_recruiter','t400_id_admin')
->>>>>>> feature/Publicar-vacantes
 		return self.queryset
   
 
