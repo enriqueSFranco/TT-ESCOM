5d20bf692153e6c4f115a8103b8b270c3fefcaca
enriqueSFranco <enrique.sfranco04@gmail.com>
2022-03-31 18:43:43 -0600

    regresando a react v17

--- frontend/src/components/Menu/DropMenu.js
+++ frontend/src/components/Menu/DropMenu.js
@@ -1,23 +1,33 @@
-import { useRef } from 'react';
+import { useEffect, useState, useRef } from "react";
 import { Link } from 'react-router-dom';
-import { useDetectClick } from '../../hooks/useDetectClick';
 import * as IoIcon from 'react-icons/io';
 import styles from './Dropdown.module.css';
 
 const DropMenu = () => {
   const dropdownRef = useRef(null);
-  const [isActive, setIsActive] = useDetectClick(dropdownRef, false);
+  const [isActive, setIsActive] = useState(false);
+
+  const onClick = ()=> setIsActive(!isActive);
+
+  useEffect(() => {
+    const pageClickEvent = (e) => {
+      if (dropdownRef.current !== null && !dropdownRef.current.contains(e.target)) {
+        setIsActive(!isActive);
+      }
+    };
+
+    if (isActive)
+      window.addEventListener('click', pageClickEvent);
+
+    return () => window.removeEventListener('click', pageClickEvent);
+  }, [isActive]);
   
-  const onClick = () => setIsActive(!isActive);
-
-  console.log(isActive)
-
   return (
     <div className={styles.dropdown}>
       <button className={styles.trigger} onClick={onClick}>
         <IoIcon.IoMdMenu style={{color: "#fff"}} />
       </button>
-      <nav ref={dropdownRef} className={`${styles.menu} ${isActive ? `${styles.active}` : `${styles.inactive}`}`}>
+      <nav ref={dropdownRef} className={`${styles.menu} ${isActive ? `${styles.active}` : ''}`}>
         <ul>
           <li><Link to="/alumno">Eres alumno ?</Link></li>
           <li><Link to="/reclutador">Eres reclutador ?</Link></li>
