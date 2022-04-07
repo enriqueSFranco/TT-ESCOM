8a7fc8986324c501149ddded2522ca2519efcfee
enriqueSFranco <enrique.sfranco04@gmail.com>
2022-04-06 20:32:51 -0500

    servicio para la peticion de crear un nuevo alumno

--- frontend/src/services/students/createAccountStudent.js
+++ frontend/src/services/students/createAccountStudent.js
@@ -1,15 +1,26 @@
 import axios from "axios";
 import { API_STUDENT } from "../settings";
 
-export const createAccountStudent = ({t100_name, t100_boleta, t100_email, t100_password} = {}) => {
-  return axios.post(API_STUDENT, {t100_name, t100_boleta, t100_email, t100_password}, {
-    headers: {
-      'Content-Type': 'application/json',
-      'Accept': 'application/json'
-    }
-  })
-    .then(response => {
+export const createAccountStudent = ({
+  t100_boleta,
+  t100_name,
+  t100_email,
+  password,
+} = {}) => {
+  return axios
+    .post(
+      API_STUDENT,
+      { t100_boleta, t100_name, t100_email, password },
+      {
+        headers: {
+          "Content-Type": "application/json",
+          accept: "application/json",
+        },
+      }
+    )
+    .then((response) => {
       const { data } = response;
       return data;
     })
+    .catch((error) => error.response);
 };
