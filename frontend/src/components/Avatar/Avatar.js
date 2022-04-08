26137bd4db04e04d82a2331496026defe73799bb
enriqueSFranco <enrique.sfranco04@gmail.com>
2022-04-07 20:54:35 -0500

    componente avatar

--- frontend/src/components/Avatar/Avatar.js
+++ frontend/src/components/Avatar/Avatar.js
@@ -5,38 +5,48 @@ import styles from "./Avatar.module.css";
 const Avatar = ({ student }) => {
   const [image, setImage] = useState(null);
 
-  const onChange = e => setImage(e.target.files[0]);
+  const onChange = (e) => setImage(e.target.files[0]);
 
   const upload = async (e) => {
     e.preventDefault();
     const formData = new FormData();
 
-    formData.append('file', image);
+    formData.append("file", image);
 
     const { data } = await axios.put(`/images/StudentPic/2014/`, formData);
     console.log(data);
-  }
+  };
 
-  // console.log(image); 
+  // console.log(student);
 
-  return (
-    <div className={styles.wrapperAvatar}>
-      <form onSubmit={upload}>
-        <input type="file" id="avatar" name="avatar" onChange={onChange} className={styles.uploadImage} />
-      </form>
-      <div className={styles.avatar}>
-        {
-          !image ? (
+  if (student !== undefined) {
+    const name = student?.t100_boleta?.t100_name;
+    const lastName = student?.t100_boleta?.t100_last_name;
+
+    return (
+      <div className={styles.wrapperAvatar}>
+        <form onSubmit={upload}>
+          <input
+            type="file"
+            id="avatar"
+            name="avatar"
+            onChange={onChange}
+            className={styles.uploadImage}
+          />
+        </form>
+        <div className={styles.avatar}>
+          {!image ? (
             <div className={styles.noImage}>
-              {student[0]?.t100_name.slice(0,1)}{student[0]?.t100_last_name.slice(0,1)}
+              {name.slice(0, 1)}{lastName && lastName.slice(0, 1)}
             </div>
           ) : (
             <div className={styles.image}>con foto</div>
-          )
-        }
+          )}
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
+  return null;
 };
 
 export default Avatar;
