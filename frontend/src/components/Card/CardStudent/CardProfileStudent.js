f658f5344297617a0e76e207b1ce8ced6df8277e
enriqueSFranco <enrique.sfranco04@gmail.com>
2022-04-07 20:55:07 -0500

    ya hace la peticion a la api de links

--- frontend/src/components/Card/CardStudent/CardProfileStudent.js
+++ frontend/src/components/Card/CardStudent/CardProfileStudent.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
-import { motion } from "framer-motion" ;
-import { getSocialNetwork } from "../../../services/students/getSocialNetwork"
+import { motion } from "framer-motion";
+import { getSocialNetwork } from "../../../services/students/getSocialNetwork";
+import { uuid } from "../../../utils/uuid";
 import FormUpdateDataStudent from "../../Form/FormUpdateDataStudent";
 import Avatar from "../../Avatar/Avatar";
 import * as MdIcon from "react-icons/md";
@@ -10,39 +11,39 @@ import styles from "./CardProfileStudent.module.css";
 
 const CardProfileStudent = () => {
   const [state, setState] = useState("profile");
-  const [student, setStudent] = useState({});
-  const [residence, setResidencce] = useState({});
-  const [linksSocialNewtworks, setLinksSocialNetworks] = useState([]);
-  
+  const [socialNetworks, setSocialNetworks] = useState([]);
+
   const handleEdit = (e) => {
     let isEdit = state === "edit" ? "profile" : "edit";
     setState(isEdit);
   };
 
   useEffect(() => {
-    getSocialNetwork("2021")
-      .then(response => {
-        setStudent(response);
+    getSocialNetwork("2017")
+      .then((response) => {
+        setSocialNetworks(response);
       })
-      .catch(error => console.log(error));
+      .catch((error) => console.log(error));
   }, []);
-  
-  if (!student) return null;
-  const [data] = student;
-  const { t113_link, t100_boleta } = data;
-  console.log(t113_link)
+
+  if (!socialNetworks) return null;
+
+  const student = socialNetworks[1];
 
   return (
     <>
       {state === "edit" ? (
-        <motion.article 
+        <motion.article
           className="container"
-          initial={{scaleY: 0}}
-          animate={{scaleY: 1}}
-          exit={{scaleY: 0}}
-          duration={{duration: 0.5}}
+          initial={{ scaleY: 0 }}
+          animate={{ scaleY: 1 }}
+          exit={{ scaleY: 0 }}
+          duration={{ duration: 0.5 }}
         >
-          <FormUpdateDataStudent student={student} handleBackToProfile={handleEdit} />
+          <FormUpdateDataStudent
+            student={student}
+            handleBackToProfile={handleEdit}
+          />
         </motion.article>
       ) : (
         <article className={`${styles.mainContainer} container`}>
@@ -54,10 +55,15 @@ const CardProfileStudent = () => {
                   onClick={handleEdit}
                 />
                 {/* <img src="https://placeimg.com/640/480/any" alt="user" /> */}
-                {/* <Avatar student={student} /> */}
+                <Avatar student={student} />
                 <div className={styles.nameHolder}>
-                  {/* <h3>{student[0]?.t100_name}<span className={styles.username}>({student[0]?.t100_username})</span></h3>
-                  <h4>{student[0]?.t100_speciality}</h4> */}
+                  <h3>
+                    {student?.t100_boleta?.t100_name}
+                    <span className={styles.username}>
+                      {student?.t100_boleta?.t100_username ?? ""}
+                    </span>
+                  </h3>
+                  <h4>{student?.t100_boleta?.t100_speciality ?? ""}</h4>
                 </div>
               </div>
             </header>
@@ -66,45 +72,37 @@ const CardProfileStudent = () => {
                 <h4 className={styles.label}>Ubicacion</h4>
                 <div className={styles.flex}>
                   <MdIcon.MdLocationPin className={styles.icon} />
-                  <p>{student[0]?.t100_residence}</p>
+                  <p>{student?.t100_boleta?.t100_residence ?? "No especificado."}</p>
                 </div>
                 <div className={styles.flex}>
                   <MdIcon.MdOutlineAirplanemodeActive className={styles.icon} />
-                  <p>Disponible para reubicarse</p>
+                  <p>
+                    {student?.t100_boleta?.travel
+                      ? "Disponible para reubicarse."
+                      : "No disponible para reubicarse." ?? "No especificado."}
+                  </p>
                 </div>
               </div>
               <div className={`${styles.socialNetworks} ${styles.separator}`}>
                 <h4 className={styles.label}>redes solicales</h4>
-                {
-                  linksSocialNewtworks.map((link) => {
-                    return (
-                      <div className={styles.flex}>
-                        <span key={Date.now()}>{link?.t113_link}</span>
-                      </div>
-                    )
-                  })
-                }
-                {/* <Link to="/" className={styles.whatsapp}>
-                  <BsIcon.BsWhatsapp />
-                  <span>2938394</span>
-                </Link>
-                <Link to="/" className={styles.email}>
-                  <MdIcon.MdAlternateEmail />
-                  <span>enrique@gmail.com</span>
-                </Link>
-                <Link to="/" className={styles.github}>
-                  <BsIcon.BsGithub />
-                  <span>enriqueSF</span>
-                </Link>
-                <Link to="/" className={styles.linkedin}>
-                  <BsIcon.BsLinkedin />
-                  <span>enrique Salinas Franco</span>
-                </Link> */}
+                {socialNetworks.map(({ t113_link, c115_id_plataform }) => {
+                  return (
+                    <a
+                      href={`${t113_link}`}
+                      target="_blank"
+                      rel="noreferrer"
+                      key={uuid()}
+                    >
+                      {c115_id_plataform?.c115_description}
+                    </a>
+                  );
+                })}
               </div>
-              <div className={`${styles.flexColumn} py-4`}>
+              <div className={`${styles.cv} py-4`}>
                 <IoIcon.IoIosCheckmarkCircle />
                 <p>
                   Tu curriculum esta activo y visible para las empresas.
+                  <br />
                   <em>Abierto a oportunidades.</em>
                 </p>
               </div>
