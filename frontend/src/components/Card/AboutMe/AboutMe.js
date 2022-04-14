93f813784555801c610299f191c26854d59b2850
enriqueSFranco <enrique.sfranco04@gmail.com>
2022-04-14 00:13:49 -0500

    se modifico el compoente que muestra la descripcion personal del alumno

--- frontend/src/components/Card/AboutMe/AboutMe.js
+++ frontend/src/components/Card/AboutMe/AboutMe.js
@@ -1,15 +1,17 @@
-import React from "react";
-import Objects from "./Objects";
-import Interests from "./Interests";
-import Languajes from "./Languajes";
+// import Objects from "./Objects";
+// import Interests from "./Interests";
+// import Languajes from "./Languajes";
 import styles from "./AboutMe.module.css";
 
-const AboutMe = () => {
+const AboutMe = ({ student }) => {
   return (
-    <div className={styles.conteiner}>
-      <Objects />
-      <Interests />
-      <Languajes />
+    <div className={styles.wrapper}>
+      <h1 className={styles.title}>Objetivo Profesional</h1>
+      <p className={styles.professionalObjective}>
+        Mi objetivo profesional es desarrollarme dentro de una empresa nacional con perspectivas a largo plazo, aplicando mis conocimientos y aptitudes comunicativas para mejorar la experiencia de los clientes y la capacidad organizativa del local
+      </p>
+      <p className={styles.salary}>Sueldo deseado: $3000</p>
+      <p className={styles.employmentModality}>Modalidad de trabajo: Home Office</p>
     </div>
   );
 };
