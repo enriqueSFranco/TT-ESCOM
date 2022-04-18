951a7209cafb494d693d2d193859680e79ad34a0
JoelHeras <joel_kratos@hotmail.com>
2022-04-17 20:33:13 -0500

    Upadate: Recruiter and Company usage

--- backend/api/api/settings.py
+++ backend/api/api/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import datetime
 from pathlib import Path
 from pickle import TRUE
 from datetime import timedelta
@@ -134,8 +135,10 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    #'ACCESS_TOKEN_LIFETIME': datetime.timedelta(days=15),
+    #'REFRESH_TOKEN_LIFETIME': datetime.timedelta(days=15),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=15),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=90),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
     'UPDATE_LAST_LOGIN': False,
--- backend/api/apps/companies/api/serializer/company_serializer.py
+++ backend/api/apps/companies/api/serializer/company_serializer.py
@@ -6,7 +6,7 @@ class CompanySerializer(serializers.ModelSerializer):
         model = Company
         fields = ('t300_id_company','t300_name','t300_rfc','t300_nss','t300_email','t300_bussiness_name',
         't300_web_page','t300_mision','t300_vision','t300_objective','t400_id_admin',
-        't300_create_date')
+        't300_create_date','t300_verified')
     
     def create(self,validate_data):
         new_company = Company(**validate_data)
@@ -32,4 +32,16 @@ class UpdateCompanySerializer(serializers.ModelSerializer):
             return u_company
 
 
+#t300_verified,t400_id_admin
+class VerifiedStateUpdate(serializers.ModelSerializer):
+        class Meta:
+            model = Company
+            exclude = ('t300_id_company','t300_name','t300_rfc','t300_nss','t300_email','t300_bussiness_name',
+            't300_web_page','t300_mision','t300_vision','t300_objective','t300_logo','t300_banner',
+            't300_create_date')
+
+        def update(self,instance,validate_data):
+            validate_company = super().update(instance,validate_data)
+            validate_company.save()
+            return validate_company
 
