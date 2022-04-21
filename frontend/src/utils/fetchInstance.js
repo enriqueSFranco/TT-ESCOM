0579cfe6feda212c449310722fbc0767a4304757
enriqueSFranco <enrique.sfranco04@gmail.com>
2022-04-21 16:57:17 -0500

    helper para obtener el token refresh

--- frontend/src/utils/fetchInstance.js
+++ frontend/src/utils/fetchInstance.js
@@ -1,60 +1,51 @@
 import axios from "axios";
-// import dayjs from "dayjs";
-// import jwt_decode from "jwt-decode";
-
 
 const originalRequest = async (url, config) => {
   let response = await axios.get(url, config);
   let { data } = response;
 
   return { response, data };
-}
+};
 
 const refreshToken = (newToken = {}) => {
-  console.log(newToken)
-  return axios.post('/api/token/refresh-token/', JSON.stringify({'refresh': newToken}), {
-    headers: {
-      'Content-Type': 'application/json',
-      'Accept': 'application/json',
-    }
-  })
-    .then(response => {
+  console.log(newToken);
+  return axios
+    .post("/api/token/refresh-token/", JSON.stringify({ refresh: newToken }), {
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    })
+    .then((response) => {
       const { data } = response;
-      window.sessionStorage.setItem('token', JSON.stringify(data));
+      window.sessionStorage.setItem("token", JSON.stringify(data));
       return data;
     })
-    .catch(error => {
+    .catch((error) => {
       if (error.response) return error.reposne.data.message;
     });
 };
 
-
 export const customFetch = async (url, config) => {
-let token = window.sessionStorage.getItem('token') ? JSON.parse(window.sessionStorage.getItem('token')) : null;
+  let token = window.sessionStorage.getItem("token")
+    ? JSON.parse(window.sessionStorage.getItem("token"))
+    : null;
 
-  // config['headers'] = {Authorization: `Bearer ${token?.access}`}
+  let { response, data } = await originalRequest(url, config);
 
-  // const student = jwt_decode(token?.access);
-  // const isExpired = dayjs.unix(student?.exp).diff(dayjs()) < 1;
-  // console.log(token)
-  
-  // if (isExpired) token = await refreshToken(token?.refresh);
+  if (response.statusText === "Unauthorized") {
+    token = await refreshToken(token);
 
-  let {response, data} = await originalRequest(url, config)
-    console.log('After Request')
+    config["headers"] = {
+      Authorization: `Bearer ${token?.access}`,
+    };
 
-    if (response.statusText === 'Unauthorized'){
-        token = await refreshToken(token)
+    let newResponse = await originalRequest(url, {
+      Authorization: `Bearer ${token?.access}`,
+    });
+    response = newResponse.response;
+    data = newResponse.data;
+  }
 
-        config['headers'] = {
-            Authorization:`Bearer ${token?.access}`
-        }
-
-        let newResponse = await originalRequest(url, {Authorization: `Bearer ${token?.access}`})
-        response = newResponse.response
-        data = newResponse.data
-
-    }
-
-    return {response, data}
-}
+  return { response, data };
+};
