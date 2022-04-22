a24da2887a98858a90aa67fc56a7ddaa1b86d60c
enriqueSFranco <enrique.sfranco04@gmail.com>
2022-04-21 22:44:47 -0500

    se refactorizo la funcion login

--- frontend/src/context/AuthContext.js
+++ frontend/src/context/AuthContext.js
@@ -1,62 +1,53 @@
 import { createContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import jwt_decode from "jwt-decode";
+import { loginService } from "services/auth/index";
 
-const AuthContext = createContext(); // creamos el contecto
+const AuthContext = createContext(); // creamos el contexto
 
 // Provider
 const AuthProvider = ({ children }) => {
-  const [user, setUser] = useState(() => window.sessionStorage.getItem('token') ? jwt_decode(window.sessionStorage.getItem('token')) : null);
-  const [token, setToken] = useState(() => window.sessionStorage.getItem('token') ? JSON.parse(window.sessionStorage.getItem('token')) : null);
+  const [user, setUser] = useState(() =>
+    window.sessionStorage.getItem("token")
+      ? jwt_decode(window.sessionStorage.getItem("token"))
+      : null
+  );
+  const [token, setToken] = useState(() =>
+    window.sessionStorage.getItem("token")
+      ? JSON.parse(window.sessionStorage.getItem("token"))
+      : null
+  );
   const [loading, setLoading] = useState(true);
   let navigate = useNavigate();
 
   const login = async (e) => {
     e.preventDefault();
-
-    try {
-      const response = await fetch('/api/token/', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          accept: 'application/json',
-        },
-        body: JSON.stringify({
-          "username": (e.target.t100_email.value).trim(),
-          "password": (e.target.password.value).trim()
-        })
+    loginService({
+      "username": e.target.t100_email.value.trim(),
+      "password": e.target.password.value.trim(),
+    })
+      .then((response) => {
+        // console.log(response)
+        if (response.status === 200 || response.status === 201) {
+          setUser(jwt_decode(response?.data?.access));
+          setToken(response?.data);
+          window.sessionStorage.setItem("token", JSON.stringify(response?.data));
+          navigate("/");
+        } else {
+          console.log(`error: ${response.error}`);
+          window.sessionStorage.removeItem('token', JSON.stringify(response?.data))
+        }
+      })
+      .catch((error) => {
+        if (error.response) console.log(error.response.data);
       });
-      if (!response.ok) {
-        let error = {
-          err: true,
-          status: response.status || "00",
-          statusText: response.statusText || "opps, ha ocurrido un error"
-        };
-        throw error;
-      }
-      const data = await response.json();
-      console.log(data)
-
-      
-      if (response.status === 200 || response.status === 201) {
-        setUser(jwt_decode(data?.access));
-        setToken(data);
-        window.sessionStorage.setItem('token', JSON.stringify(data))
-        navigate('/');
-      } else {
-        console.log(`error: ${response.error}`);
-        window.sessionStorage.removeItem('token', JSON.stringify(data))
-      }
-    } catch (error) {
-      console.log(error);
-    }
   };
 
   const logout = () => {
     setUser(null);
     setToken(null);
-    window.sessionStorage.removeItem('token');
-    navigate('/');
+    window.sessionStorage.removeItem("token");
+    navigate("/");
   };
 
   const data = {
@@ -67,16 +58,15 @@ const AuthProvider = ({ children }) => {
   };
 
   useEffect(() => {
-    if (token)
-      setUser(jwt_decode(token?.access));
+    if (token) setUser(jwt_decode(token?.access));
     setLoading(false);
   }, [token, loading]);
 
   return (
     <AuthContext.Provider value={data}>
-      {loading ? null : children }
+      {loading ? null : children}
     </AuthContext.Provider>
-  )
+  );
 };
 
 export { AuthProvider };
