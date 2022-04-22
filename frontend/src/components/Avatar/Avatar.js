9ada93a1b70c93f405042c1c04238a1025bd7c99
enriqueSFranco <enrique.sfranco04@gmail.com>
2022-04-21 22:41:36 -0500

    componente avatar

--- frontend/src/components/Avatar/Avatar.js
+++ frontend/src/components/Avatar/Avatar.js
@@ -1,59 +1,69 @@
-import axios from "axios";
-import React, { useState } from "react";
-import styles from "./Avatar.module.css";
+// import { useState } from "react";
+import Avatar from "@mui/material/Avatar";
+// import axios from "axios";
+// import styles from "./Avatar.module.css";
 
-// ruta para subir la imagen de perfil para un alumno: /images/StudentPic/2017/
+const CustomAvatar = ({student = [], width, height, fontSize}) => {
+  console.log(student);
 
-const Avatar = ({ student }) => {
-  const [profilePicture, setProfilePicture] = useState(null);
+  if (student.length < 0) return null;
 
-  const onChange = (e) => setProfilePicture(e.target.files[0]);
+  return (
+    <Avatar sx={{ width: width, height: height, fontSize: fontSize }}>
+      {student[0]?.t100_name && student[0]?.t100_name.slice(0,1)}{student[0]?.t100_last_name && student[0]?.t100_last_name.slice(0,1)}
+    </Avatar>
+  )
+}
 
-  const upload = async (e) => {
-    e.preventDefault();
-    try {
-      const formData = new FormData();
-      const id = student[0]?.t100_boleta;
+// const Avatar = ({ student = [], width, height }) => {
+//   const [profilePicture, setProfilePicture] = useState(null);
+
+//   const onChange = (e) => setProfilePicture(e.target.files[0]);
+
+//   const upload = async (e) => {
+//     e.preventDefault();
+//     try {
+//       const formData = new FormData();
+//       const id = student[0]?.t100_boleta;
       
-      formData.append("profile_picture", profilePicture, profilePicture.name);
-      const { data } = await axios.put(`/images/StudentPic/${id}/`, formData);
+//       formData.append("profile_picture", profilePicture, profilePicture.name);
+//       const { data } = await axios.put(`/images/StudentPic/${id}/`, formData);
   
-      console.log(data);
-    } catch (error) {
-      console.log(error);
-    }
-  };
+//       // console.log(data);
+//     } catch (error) {
+//       // console.log(error);
+//     }
+//   };
 
-  if (student.length > 0) {
-    const name = student[0]?.t100_name;
-    const lastName = student[0]?.t100_last_name;
+//   if (student.length > 0) {
+//     const name = student[0]?.t100_name;
+//     const lastName = student[0]?.t100_last_name;
 
-    return (
-      <div className={styles.wrapperAvatar}>
-        <form onSubmit={upload} className={styles.form}>
-          <input
-            accept="image/*"
-            type="file"
-            id="avatar"
-            name="avatar"
-            onChange={onChange}
-            className={styles.uploadImage}
-          />
-        </form>
-        <div className={styles.avatar}>
-          {!profilePicture ? (
-            <div className={styles.noImage}>
-              {name.slice(0, 1)}{lastName && lastName.slice(0, 1)}
-            </div>
-          ) : (
-            // <img src={} alt={}>
-            <div className={styles.image}>con foto</div>
-          )}
-        </div>
-      </div>
-    );
-  }
-  return null;
-};
+//     return (
+//       <div className={styles.wrapperAvatar} style={{width: width, height: height}}>
+//         <form onSubmit={upload} className={styles.form}>
+//           <input
+//             accept="image/*"
+//             type="file"
+//             id="avatar"
+//             name="avatar"
+//             onChange={onChange}
+//             className={styles.uploadImage}
+//           />
+//         </form>
+//           {!profilePicture ? (
+//             <div className={styles.noImage}>
+//               {name.slice(0, 1)}{lastName && lastName.slice(0, 1)}
+//             </div>
+//           ) : (
+//             <div className={styles.image}>
+//               {/* <img src={} alt={}> */}
+//             </div>
+//           )}
+//       </div>
+//     );
+//   }
+//   return null;
+// };
 
-export default Avatar;
+export default CustomAvatar;
