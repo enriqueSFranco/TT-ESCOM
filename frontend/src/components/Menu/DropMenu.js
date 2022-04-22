9c3dcc0ecb2720992a0e9a1943190dd6d13334c5
enriqueSFranco <enrique.sfranco04@gmail.com>
2022-04-21 22:43:25 -0500

    componente dropdown

--- frontend/src/components/Menu/DropMenu.js
+++ frontend/src/components/Menu/DropMenu.js
@@ -1,45 +1,32 @@
 import { useContext, useRef } from "react";
 import { Link } from "react-router-dom";
-import AuthContext from "context/AuthContext";
 import { useDetectClick } from "hooks/useDetectClick";
-import * as IoIcon from "react-icons/io";
+import AuthContext from "context/AuthContext";
+import { IoMdMenu } from "react-icons/io";
 import styles from "./Dropdown.module.css";
+import DropMenuStudent from "./DropMenuStudent";
 
 const DropMenu = () => {
-  const { user, logout } = useContext(AuthContext);
+  const { user } = useContext(AuthContext);
   const dropdownRef = useRef(null);
   const [isActive, setIsActive] = useDetectClick(dropdownRef, false);
 
   const onClick = () => setIsActive(!isActive);
+  
+  // if (user === null) return null;
 
   return (
     <div className={styles.dropdown}>
       <button className={styles.trigger} onClick={onClick}>
-        <IoIcon.IoMdMenu style={{ color: "#fff" }} />
+        <IoMdMenu style={{ color: "#fff" }} />
       </button>
       <nav
         ref={dropdownRef}
         className={`${styles.menu} ${isActive ? `${styles.active}` : ""}`}
       >
-        <ul>
+        <ul className={styles.menuList}>
           {user ? (
-            <>
-              <li>
-                <Link to="/perfil">Perfil</Link>
-              </li>
-              <li>
-                <Link to="/">Mis Postulaciones</Link>
-              </li>
-              <li>
-                <Link to="/">Modo Oscuro</Link>
-              </li>
-              <li>
-                <Link to="/">Configuracion</Link>
-              </li>
-              <li>
-                <Link to="/" onClick={logout}>Cerrar sesion</Link>
-              </li>
-            </>
+            <DropMenuStudent student={user} />
           ) : (
             <>
               <li>
