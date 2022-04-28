31380484a48eed83086e62a1381309edb7dcffa2
enriqueSFranco <enrique.sfranco04@gmail.com>
2022-04-27 22:05:22 -0500

    nuevas paginas

---
+++ frontend/src/pages/student/PageApplicationsStudent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './PageApplicationsStudent.module.css';
+
+const PageApplicationsStudent = () => {
+  return (
+    <section className={styles.wrapper}>
+      <h1>PageApplicationsStudent</h1>
+    </section>
+  )
+};
+
+export default PageApplicationsStudent;
