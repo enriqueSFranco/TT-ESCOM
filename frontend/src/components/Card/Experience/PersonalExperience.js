d6ae68ccb0d0a2ba335cd9629b9e87ad6eacfdf8
enriqueSFranco <enrique.sfranco04@gmail.com>
2022-05-09 20:02:31 -0500

    componente experiencia

--- frontend/src/components/Card/Experience/PersonalExperience.js
+++ frontend/src/components/Card/Experience/PersonalExperience.js
@@ -1,22 +1,41 @@
-import React from 'react';
-import styles from './Experience.module.css';
+import { TextField } from "@mui/material";
+import React from "react";
+import styles from "./Experience.module.css";
 
 const PersonalExperience = ({ setTypeProject }) => {
-
   const handleClick = () => setTypeProject(null);
 
   return (
     <div className={styles.wrapperForm}>
       <h2 className={styles.titleModal}>Experiencia por proyecto</h2>
       <form className={styles.form}>
-        <input type="text" placeholder='nombre de tu proyecto' />
-        <textarea name="" id="" cols="30" rows="10" placeholder='descripcion de tu proyecto'></textarea>
-        <input type="text" placeholder='enlace de tu proyecto' />
+        <div className={styles.inputGroup}>
+          <TextField
+            sx={{ width: "200px" }}
+            label="Nombre de tu proyecto"
+            id="outlined-size-small"
+            size="small"
+          />
+          <TextField
+            sx={{ width: "200px" }}
+            label="Enlace de tu proyecto"
+            id="outlined-size-small"
+            size="small"
+          />
+        </div>
+        <textarea
+          name=""
+          id=""
+          cols="30"
+          rows="10"
+          placeholder="descripcion de tu proyecto"
+        ></textarea>
+        
         <input type="submit" value="Agregar" />
       </form>
       <button onClick={handleClick}>Regresar</button>
     </div>
-  )
-}
+  );
+};
 
-export default PersonalExperience
+export default PersonalExperience;
