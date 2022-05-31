730d49976e0663d551a159f61f7cbcd05f2a0548
JoelHeras <joel_kratos@hotmail.com>
2022-05-31 14:56:15 -0500

    UPdate

--- backend/api/api/settings.py
+++ backend/api/api/settings.py
@@ -80,7 +80,7 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 5,
+    'PAGE_SIZE': 15,
 }
 
 ROOT_URLCONF = 'api.urls'
