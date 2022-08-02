0c66439a2fde566ed53d8b3ae0bdf04190ead046
JoelHeras <joel_kratos@hotmail.com>
2022-08-01 21:46:01 -0500

    Update backend: Fix router management

--- backend/api/api/urls.py
+++ backend/api/api/urls.py
@@ -59,5 +59,4 @@ urlpatterns = [
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-    path('api/', include('apps.routers')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
