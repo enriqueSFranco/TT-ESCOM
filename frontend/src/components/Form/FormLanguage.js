922ebca7d055f392aaa1cba250aeef65a93741a9
enriqueSFranco <enrique.sfranco04@gmail.com>
2022-09-27 18:57:32 -0500

    v2

--- frontend/src/components/Form/FormLanguage.js
+++ frontend/src/components/Form/FormLanguage.js
@@ -1,24 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useLanguage } from 'hooks'
+import Input from 'components/Input/Input'
 import Switch from 'components/Switch/Switch'
-import { Form } from './styled-components/FormLanguageStyled'
+import { Button, Form } from './styled-components/FormLanguageStyled'
 
 const FormLanguage = () => {
   const { languages, error } = useLanguage()
+  const [value, setValue] = useState(0)
+  const [hasCertification, setHasCertification] = useState(false)
+
+  const handleChange = () => setHasCertification(!hasCertification)
+
+  const handleChangePicker = (e) => setValue(e.target.value)
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log('formulario enviado...')
+  }
 
   if (!languages) return null
 
-  console.log(languages)
-
   return (
-    <Form>
+    <Form onSubmit={handleSubmit}>
       <select name="" id="">
         <option value="" disabled>Selecciona un idioma o dialecto</option>
         {languages?.map(language => (
           <option value={language?.c111_id_lenguage}>{language?.c111_description}</option>
         ))}
       </select>
-      <Switch label='Cuentas con alguna certificacion?' />
+      <div style={{display: 'flex', gap: '1rem'}}>
+        <input type="range" min={0} max={100} value={value} onChange={e => handleChangePicker(e)} />
+        <span>{value}</span>
+      </div>
+
+      <Switch label='Cuentas con alguna certificacion?' id='hasCertification' name='hasCertification' value={hasCertification} onChange={handleChange} />
+      {hasCertification && (
+        <div style={{display: 'flex', alignItems: 'center', gap: '.5rem'}}>
+          <Input label='Nombre de la certificacion' />
+          <span>de</span>
+          <Input label='Donde' />
+        </div>
+      )}
+      <Button type="submit" value='Agregar idioma' />
     </Form>
   )
 }
--- frontend/src/components/Form/styled-components/FormLanguageStyled.js
+++ frontend/src/components/Form/styled-components/FormLanguageStyled.js
@@ -1,5 +1,27 @@
 import styled from 'styled-components'
 
-const Form = styled.form``
+const Form = styled.form`
+  height: 500px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 1rem;
+`
 
-export { Form }
+const Button = styled.input`
+  width: fit-content;
+  outline: none;
+  border: none;
+  background-color: #116BFE;
+  color: #FFF;
+  border-radius: 4px;
+  padding: .5rem;
+  transition: background-color .3s ease;
+
+  &:hover {
+    /* background-color:  */
+  }
+`
+
+export { Button, Form }
