6098815ea62e8f1da5f6420609db458becc8b678
JoelHeras <joel_kratos@hotmail.com>
2022-09-28 02:07:02 -0500

    Update backend: Fix lenguages json data

--- backend/api/apps/students/api/serializer/studentlenguage_serializer.py
+++ backend/api/apps/students/api/serializer/studentlenguage_serializer.py
@@ -18,18 +18,6 @@ class LenguagesListSerializer(serializers.ModelSerializer):
         fields = '__all__'
         depth = 2
 
-    """def to_representation(self,instance):
-        print(instance)        
-        return{
-            't110_id_registrer' : instance['t110_id_registrer'],
-            't100_boleta' : instance['t100_boleta'],
-	        'c111_id_language' : instance['c111_id_language'],
-	        't110_written_level' : instance['t110_written_level'],
-	        't110_reading_level' : instance['t110_reading_level'],
-	        't110_speaking_level' : instance['t110_speaking_level'],
-	        't110_comprension_level' : instance['t110_comprension_level'],
-	        't110_native' : instance['t110_native']
-        }"""
 
 class UpdateLenguagesSerializer(serializers.ModelSerializer):
         class Meta:
