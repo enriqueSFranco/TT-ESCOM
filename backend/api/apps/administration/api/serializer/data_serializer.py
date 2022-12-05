e9def689c2caefd35d7c8bc81b6fb81203cf326e
JoelHeras <joel_kratos@hotmail.com>
2022-12-05 14:51:54 -0600

    Update backend: Update Vacant model

--- backend/api/apps/administration/api/serializer/data_serializer.py
+++ backend/api/apps/administration/api/serializer/data_serializer.py
@@ -86,4 +86,4 @@ class OnHoldVacantsSerializer(serializers.ModelSerializer):
 class ValidateVacantSerializer(serializers.ModelSerializer):
     class Meta:
         model = Vacant
-        fields = ('t400_id_admin','c204_id_vacant_status','t200_close_date')       
+        fields = ('t400_id_admin','c204_id_vacant_status','t200_close_date','t200_publish_date')       
