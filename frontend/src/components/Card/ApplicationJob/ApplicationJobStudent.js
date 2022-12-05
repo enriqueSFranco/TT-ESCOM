267a41235ed6e568b182eca64a24fd4af2e3d415
enriqueSFranco <enrique.sfranco04@gmail.com>
2022-12-05 14:06:42 -0600

    v2

--- frontend/src/components/Card/ApplicationJob/ApplicationJobStudent.js
+++ frontend/src/components/Card/ApplicationJob/ApplicationJobStudent.js
@@ -1,29 +1,27 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 import { numberFormat, formatDate } from 'utils';
 import Chip from 'components/Chip/Chip'
+import { TiLocation } from 'react-icons/ti'
+import { FaBrain } from 'react-icons/fa'
 import './ApplicationJobStudent.css';
 
-function replaceBackSpace(string) {
-  return string.replace(' ', '-').toLowerCase()
-}
-
-const ApplicationJobStudent = ({nameJob, salary, modality, dateApplication, experience, state}) => {
-
+const ApplicationJobStudent = ({nameJob, salary, modality, dateApplication, experience, contract}) => {
   return (
     <article className={`wrapper`}>
       <div>
         <h2 className='nameJob'>{nameJob}</h2>
+        <span>Contratacion: {contract}</span>
       </div>
       <div className='tags'>
         <Chip label={`$${numberFormat(salary).slice(4,)}`} bg='var(--bg-color_1)' color='var(--color_1)' />
-        <Chip label={modality ? "Presencial" : "Remoto"} bg='var(--bg-color_2)' color='var(--color_2)' />
-        <Chip label={`Experiencia:${experience}`} bg='var(--bg-color_3)' color='var(--color_3)' />
+        <Chip label={modality ? "Presencial" : "Remoto"} bg='var(--bg-color_2)' color='var(--color_2)' icon={<TiLocation style={{fontSize: "1rem"}} />} />
+        <Chip label={`${experience}`} bg='var(--bg-color_3)' color='var(--color_3)' icon={<FaBrain style={{fontSize: "1rem"}} />} />
       </div>
       <div className='flex_container'>
         <span className='applicationDate'>Fecha de postulacion: {formatDate(dateApplication)}</span>
-        <span className={`${replaceBackSpace(state)} tag-state`}>Postulacion: {state}</span>
+        <Link to="/mis-postulaciones">Ver detalles</Link>
       </div>
-      {/* <p className='paragraph'>Horario: <span>{workingHours}</span></p> */}
     </article>
   )
 };
--- frontend/src/pages/student/PageApplicationsStudent.js
+++ frontend/src/pages/student/PageApplicationsStudent.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { useFetch } from "hooks/useFetch";
 import { uuid } from "utils/uuid";
-import { formatDate } from "utils/formatDate";
 import { useAuth } from "context/AuthContext";
 import ApplicationJobStudent from "components/Card/ApplicationJob/ApplicationJobStudent";
 import LayoutHome from "Layout/LayoutHome";
@@ -17,8 +16,12 @@ import {
   tableCellClasses,
 } from "@mui/material";
 import { IoIosBusiness } from "react-icons/io";
-import { Container } from '../styled-components/ApplicationStudentStyled'
-import styles from "./PageApplicationsStudent.module.css";
+import { Container } from "../styled-components/ApplicationStudentStyled";
+import "./PageApplicationsStudent.css";
+
+function replaceBackSpace(string) {
+  return string.replace(' ', '-').toLowerCase()
+}
 
 const PageApplicationsStudent = () => {
   const { token } = useAuth();
@@ -28,11 +31,7 @@ const PageApplicationsStudent = () => {
 
   const StyledTableCell = styled(TableCell)(({ theme }) => ({
     [`&.${tableCellClasses.head}`]: {
-      background: 'hsla(217, 100%, 50%, 1)',
-      background:'linear-gradient(90deg, hsla(217, 100%, 50%, 1) 0%, hsla(186, 100%, 69%, 1) 100%)',
-      background: '-moz-linear-gradient(90deg, hsla(217, 100%, 50%, 1) 0%, hsla(186, 100%, 69%, 1) 100%)',
-      background: '-webkit-linear-gradient(90deg, hsla(217, 100%, 50%, 1) 0%, hsla(186, 100%, 69%, 1) 100%)',
-      filter: 'progid: DXImageTransform.Microsoft.gradient( startColorstr="#0061FF", endColorstr="#60EFFF", GradientType=1 )',
+      backgroundColor: "#000",
       color: theme.palette.common.white,
     },
     [`&.${tableCellClasses.body}`]: {
@@ -61,22 +60,26 @@ const PageApplicationsStudent = () => {
                   Ubicacion de la vacante
                 </StyledTableCell>
                 <StyledTableCell style={{ fontSize: "1.2rem" }} align="center">
-                  Fecha de postulacion
+                  Estado de la postulacion
                 </StyledTableCell>
               </TableRow>
             </TableHead>
             <TableBody>
               {data?.map((row) => (
                 <TableRow key={uuid()}>
-                  <TableCell sx={{ width: 150 }} style={{ textAlign: "center" }}>
-                    {row?.t200_id_vacant?.t300_id_company?.t300_logo !== null ? (
+                  <TableCell
+                    sx={{ width: 150 }}
+                    style={{ textAlign: "center" }}
+                  >
+                    {row?.t200_id_vacant?.t300_id_company?.t300_logo !==
+                    null ? (
                       <img
                         src={row?.t200_id_vacant?.t300_id_company?.t300_logo}
                         alt={row?.t200_id_vacant?.t300_id_company}
-                        className={styles.image}
+                        className="image"
                       />
                     ) : (
-                      <IoIosBusiness className={styles.noImage} />
+                      <IoIosBusiness className="noImage" />
                     )}
                   </TableCell>
                   <TableCell
@@ -90,18 +93,22 @@ const PageApplicationsStudent = () => {
                       salary={row?.t200_id_vacant?.t200_max_salary}
                       modality={row?.t200_id_vacant?.t200_home_ofice}
                       experience={
-                        row?.t200_id_vacant?.c207_id_experience?.c207_description
+                        row?.t200_id_vacant?.c207_id_experience
+                          ?.c207_description
                       }
                       dateApplication={row?.t201_date_application}
-                      state={row?.c205_id_application_state?.c205_description}
+                      contract={row?.t200_id_vacant?.c208_id_contract?.c208_description}
                     />
                   </TableCell>
                   <TableCell
-                    style={{ fontSize: "1rem", textAlign: 'center' }}
+                    style={{ fontSize: "1rem", textAlign: "center" }}
                     sx={{ width: 350 }}
                   >{`${row?.t200_id_vacant?.c222_id_locality?.c222_municipality}, ${row?.t200_id_vacant?.c222_id_locality?.c222_state}, ${row?.t200_id_vacant?.t200_street}`}</TableCell>
                   <TableCell style={{ fontSize: "1rem" }} align="center">
-                    {formatDate(row?.t201_date_application)}
+                    <div className="tag_state">
+                      <div className={`${replaceBackSpace(row?.c205_id_application_state?.c205_description)} circle_state`}></div>
+                      {row?.c205_id_application_state?.c205_description}
+                    </div>
                   </TableCell>
                 </TableRow>
               ))}
