8b749cef2e40a86dabda1576599a3337e2290f28
enriqueSFranco <enrique.sfranco04@gmail.com>
2023-01-05 12:24:29 -0600

    v2

--- frontend/src/pages/EmptyView.js
+++ frontend/src/pages/EmptyView.js
@@ -4,8 +4,13 @@ import noResults from "assets/images/plane_2.gif";
 
 const TextH3 = styled.h3`
   text-align: center;
-  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-  background: linear-gradient(27deg, #3f5efb, #fc466b);
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+    Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+  background: linear-gradient(
+    90deg,
+    hsla(217, 100%, 50%, 1) 0%,
+    hsla(186, 100%, 69%, 1) 100%
+  );
   background-clip: text;
   -webkit-background-clip: text;
   color: transparent;
