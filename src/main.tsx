bb881a6a705132531b79d673ba4e233fd314b44e
enriqueSFranco <enrique.sfranco04@gmail.com>
2023-08-28 13:58:38 -0600

    feat: Add provider redux

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { createRoot } from "react-dom/client"
 import { RouterProvider } from "react-router-dom"
 import { router } from './Root'
+import store from './stores'
+import { Provider } from 'react-redux'
 import './index.css'
 
 const $app = document.getElementById('app') as HTMLElement
@@ -9,7 +11,9 @@ const root = createRoot($app)
 
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
 )
 
